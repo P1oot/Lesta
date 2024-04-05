649cca6eb5b2f2951844847c712547953c887bf5
Dan <dan.berdinskikh@mail.ru>
2024-04-05 23:50:24 +0300

    Add file and words admin

--- lesta/text_app/admin.py
+++ lesta/text_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import File, Words
 
-# Register your models here.
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    model = File
+    list_display = ('file', 'upload_at', 'processed')
+
+
+@admin.register(Words)
+class WordsAdmin(admin.ModelAdmin):
+    model = Words
+    list_display = ('word', 'tf')
