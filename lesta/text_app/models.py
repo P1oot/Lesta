2ad9b2cfe7217b64c74df62890a229663ab78db2
Dan <dan.berdinskikh@mail.ru>
2024-04-05 23:49:51 +0300

    Add file and words model

--- lesta/text_app/models.py
+++ lesta/text_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField(
+        verbose_name='Файл',
+        upload_to='files/',
+    )
+    uplode_at = models.DateTimeField(
+        verbose_name='Время загрузки',
+        auto_now_add=True,
+    )
+    processed = models.BooleanField(
+        verbose_name='Статус загрузки',
+        default=False,
+    )
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_pural = 'Файлы'
