3eecebc738313a21284043fa4913fcf7132c3e23
Dan <dan.berdinskikh@mail.ru>
2024-04-07 16:27:48 +0300

    Add base viewsets

--- lesta/text_app/views.py
+++ lesta/text_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from .models import File, Words
+from .serializers import FileSerializer, WordsSerializer
 
-# Create your views here.
+
+class FileViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+
+class WordsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Words.objects.all()
+    serializer_class = WordsSerializer
