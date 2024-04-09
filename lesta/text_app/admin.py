22c49c75d4aee4dfc09f973f372e52b33ce21826
Dan <dan.berdinskikh@mail.ru>
2024-04-09 12:36:46 +0300

    Del WordsAdmin

--- lesta/text_app/admin.py
+++ lesta/text_app/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import File, Words
+from .models import File
 
 
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
     model = File
-    list_display = ('file', 'upload_at', 'processed')
-
-
-@admin.register(Words)
-class WordsAdmin(admin.ModelAdmin):
-    model = Words
-    list_display = ('word', 'tf')
+    list_display = ('file', 'name', 'upload_at', 'processed')
