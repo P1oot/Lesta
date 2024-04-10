35e81fdfb1933d7e90291652909fbe036bf8de1a
Dan <dan.berdinskikh@mail.ru>
2024-04-11 02:39:46 +0300

    Add index and upload pages

--- lesta/text_app/urls.py
+++ lesta/text_app/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from .views import FileUploadViewSet, FileListRetrieveViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import (
+    FileUploadViewSet,
+    FileListRetrieveViewSet,
+    index,
+    upload_file,
+)
+
+app_name = 'text_app'
 
 router = SimpleRouter()
-router.register('api/upload', FileUploadViewSet, basename='upload')
-router.register('api/files', FileListRetrieveViewSet, basename='files')
+router.register('upload', FileUploadViewSet, basename='upload')
+router.register('files', FileListRetrieveViewSet, basename='files')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('api/', include(router.urls)),
+    path('', index, name='index'),
+    path('upload/', upload_file, name='upload_file')
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
--- lesta/text_app/views.py
+++ lesta/text_app/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, mixins
-# from rest_framework.response import Response
 from .models import File
 from .serializers import (FileUploadSerializer,
                           FileListSerializer,
                           FileRetrieveSerializer)
 from .tasks import file_processing
-# from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FileForm
+import requests
 
 
 class FileUploadViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -23,9 +24,39 @@ class FileListRetrieveViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet
 ):
-    queryset = File.objects.all()
+    queryset = File.objects.all().order_by('-upload_at')
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return FileRetrieveSerializer
         return FileListSerializer
+
+
+def index(request):
+    template = 'index.html'
+    files_list = File.objects.all().order_by('-upload_at')
+    context = {
+        'files': files_list,
+    }
+    return render(request, template, context)
+
+
+def upload_file(request):
+    template = 'upload.html'
+    form = FileForm(request.POST or None, files=request.FILES or None)
+    if request.method == 'POST':
+        file = request.FILES['file']
+        print(type(file))
+        r = requests.post(
+            'http://127.0.0.1:8000/api/upload/',
+            files=[('file', file)],
+            # data={'file': file},
+        )
+        if r.status_code == 201:
+            return redirect('text_app:index')
+        else:
+            pass
+            return render(request, template, {'form': form,
+                                              'warning': True})
+    else:
+        return render(request, template, {'form': form})
